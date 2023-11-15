5f68be69d255db3b3f6eab73a40bd1728b6143c5
NotAShelf <raf@notashelf.dev>
2023-11-15 10:52:24 +0300

    pkgs/arrpc: 2023-10-17-unstable -> 2023-11-01-unstable

--- package.nix
+++ package.nix
@@ -4,44 +4,45 @@
   buildNpmPackage,
   nodejs,
   extraFlags ? [],
-}:
-buildNpmPackage {
+}: let
   pname = "arRPC";
-  version = "2023-19-17-unstable";
+  version = "2023-11-01-unstable";
+in
+  buildNpmPackage {
+    inherit pname version;
 
-  src = fetchFromGitHub {
-    owner = "OpenAsar";
-    repo = "arRPC";
-    rev = "89f4da610ccfac93f461826a446a17cd3b23953d";
-    hash = "sha256-M4oQBpY/t+MEyvCienQE/GU2JfFzPKI9U2jQTRZQs4I=";
-  };
+    src = fetchFromGitHub {
+      owner = "OpenAsar";
+      repo = "arRPC";
+      rev = "c902d82addb4a84ede7e8dd4ccfe796570c05254";
+      hash = "sha256-wuhFD8rQEyFz/u/Y4p5QHEsDB8b0UHOTl+QWKF4+wAQ=";
+    };
 
-  dontNpmBuild = true;
+    dontNpmBuild = true;
+    npmDepsHash = "sha256-vxx0w6UjwxIK4cgpivtjNbIgkb4wKG4ijSHdP/FeQZ4=";
 
-  npmDepsHash = "sha256-vxx0w6UjwxIK4cgpivtjNbIgkb4wKG4ijSHdP/FeQZ4=";
+    preInstall = ''
+      mkdir -p $out/lib/node_modules/arRPC/
+    '';
 
-  preInstall = ''
-    mkdir -p $out/lib/node_modules/arRPC/
-  '';
+    postInstall = ''
+      cp -rf src/* ext/* $out/lib/node_modules/arRPC/
+    '';
 
-  postInstall = ''
-    cp -rf src/* ext/* $out/lib/node_modules/arRPC/
-  '';
+    postFixup = ''
+      ${nodejs}/bin/node --version
+      makeWrapper ${nodejs}/bin/node $out/bin/arRPC \
+        --add-flags $out/lib/node_modules/arrpc/src \
+        --chdir $out/lib/node_modules/arrpc/src \
+        ${lib.concatStringsSep " " (map (flag: "--add-flags ${flag}") extraFlags)}
+    '';
 
-  postFixup = ''
-    ${nodejs}/bin/node --version
-    makeWrapper ${nodejs}/bin/node $out/bin/arRPC \
-      --add-flags $out/lib/node_modules/arrpc/src \
-      --chdir $out/lib/node_modules/arrpc/src \
-      ${lib.concatStringsSep " " (map (flag: "--add-flags ${flag}") extraFlags)}
-  '';
-
-  meta = with lib; {
-    mainProgram = "arRPC";
-    description = "Open Discord RPC server for atypical setups ";
-    homepage = "https://github.com/OpenAsar/arRPC";
-    changelog = "https://github.com/OpenAsar/arRPC/blob/${version}/changelog.md";
-    license = licenses.mit;
-    maintainers = with maintainers; [notashelf];
-  };
-}
+    meta = with lib; {
+      changelog = "https://github.com/OpenAsar/arRPC/blob/main/changelog.md";
+      description = "Open Discord RPC server for atypical setups ";
+      homepage = "https://github.com/OpenAsar/arRPC";
+      license = licenses.mit;
+      maintainers = with maintainers; [notashelf];
+      mainProgram = pname;
+    };
+  }
